ca696fb68ea7c9af71090a95057792156098a74f
Marwan Ashraf <xmarwanashraf@gmail.com>
2023-09-25 09:55:14 +0300

    First

---
+++ script.js
@@ -0,0 +1,33 @@
+let myspans = document.querySelectorAll(".rating span")
+let btn = document.querySelector("button");
+let myrate = document.querySelector(".after-submit p span")
+
+btn.addEventListener("click",function(e){
+    myspans.forEach(function(span){
+        if(!span.classList.contains("change-color-span")){
+            e.preventDefault();
+        }else {
+            document.querySelector(".main").style.display = "none";
+        document.querySelector(".after-submit").style.display = "flex";
+        }
+    })
+})
+myspans.forEach(function(span){
+    span.addEventListener("click",function(e){
+        for(let i = 0; i<myspans.length; i++){
+            if(e.target === myspans[i]){
+                if(myspans[i].classList.contains("change-color-span")){
+                    myspans[i].classList.remove("change-color-span")
+                }else {
+                    myspans[i].classList.add("change-color-span")
+                    myrate.innerHTML = myspans[i].innerHTML || "0";
+                }
+            }else if(e.target !== myspans[i]){
+                if(myspans[i].classList.contains("change-color-span")){
+                    myspans[i].classList.remove("change-color-span")
+                }
+            }
+        }
+    })
+})
+
